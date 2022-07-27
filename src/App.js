45fe99190ac62b0b597d3634c2fe1bfed6bf56f6
OsamaAtiq12 <osama.517@live.com>
2022-07-27 16:47:21 +0500

    Todo-list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Todoform from "./Components/Todoform";
+
 import Todolist from "./Components/Todolist";
-import "./App.css"; //
+// import "./App.css"; //
 function App() {
   return (
     <div className="todo-app">
